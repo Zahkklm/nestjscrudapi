000cdf7dbb1271cf77f42a827f1bbdc6d5515c26
Zahkklm <ozgurpeynirci@gmail.com>
2024-06-01 09:37:37 +0300

    Add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({ name: 'users' })
+export class Users {
+    @PrimaryGeneratedColumn('uuid', { name: 'id' })
+    id: string;
+    @Column()
+    username: string;
+    @Column()
+    password: string;
+}
