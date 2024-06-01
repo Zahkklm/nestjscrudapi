356a73e194c5d3b402cc264052de61cfd51bff24
Zahkklm <ozgurpeynirci@gmail.com>
2024-06-01 19:31:48 +0300

    delete auto generated module for readable project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { EmailVerifierModule } from './email-verifier/email-verifier.module';
-import { EmailVerifierController } from './email-verifier/email-verifier.controller';
-import { EmailVerifierService } from './email-verifier/email-verifier.service';
 import { UserModule } from './users/users.module';
 import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, EmailVerifierModule, UserModule, MailModule, ConfigModule.forRoot({isGlobal: true})],
-  controllers: [AppController, EmailVerifierController],
-  providers: [AppService, EmailVerifierService],
+  imports: [DatabaseModule, UserModule, MailModule, ConfigModule.forRoot({isGlobal: true})],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/email-verifier/email-verifier.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmailVerifierController } from './email-verifier.controller';
-import { EmailVerifierService } from './email-verifier.service';
-
-@Module({
-  controllers: [EmailVerifierController],
-  providers: [EmailVerifierService]
-})
-export class EmailVerifierModule {}
