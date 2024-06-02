07e3eb416c811455d26550136e7842f65893ba75
Zahkklm <ozgurpeynirci@gmail.com>
2024-06-02 18:05:21 +0300

    Remove database module from .gitignore since credentials are stored in .env

---
+++ src/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from '../config/database.config';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot(databaseConfig()),
+    ],
+})
+export class DatabaseModule { }
