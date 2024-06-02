93d9ce3caaa76ef917e8d04bd543a404525214a8
Zahkklm <ozgurpeynirci@gmail.com>
2024-06-02 18:51:13 +0300

    Include both database and SMTP configuration in same .env file Add regarding instructions to README.md

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,8 +5,19 @@ import databaseConfig from '../config/database.config';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRoot(databaseConfig()),
+        ConfigModule.forRoot({isGlobal: true}),
+        TypeOrmModule.forRootAsync(
+            {
+                inject: [ConfigService],
+                useFactory: async (configService: ConfigService) => ({
+                    type: configService.get('DB_TYPE'), // postgres
+                    url: configService.get('DB_URL'), // connection string
+                    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                    synchronize: true, 
+                }),
+                
+            }
+        ),
     ],
 })
 export class DatabaseModule { }
