325aef90db410577fc2d10c26e523feb00d229e1
Zahkklm <ozgurpeynirci@gmail.com>
2024-06-02 22:40:36 +0300

    Add End to End test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('UserController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,29 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/user/register (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/user/register')
+      .send({ username: 'testuser', email: 'test@example.com' })
+      .expect(201);
+
+    expect(response.body.username).toEqual('testuser');
+    expect(response.body.email).toEqual('test@example.com');
+  });
+
+  it('/user/verify-email/:username/:verificationToken', async () => {
+    await request(app.getHttpServer())
+      .get('/user/verify-email/testuser/testtoken+%79123&-@')
+      .expect(404);
+  });
+
+  it('/user/check-verification/:username (GET)', async () => {
+    await request(app.getHttpServer())
+      .get('/user/check-verification/testuser')
+      .expect(404);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
